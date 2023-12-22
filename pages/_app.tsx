1cd6e0fc381c534ab65b1968563e18e1f4496ca4
CJ <camillus@jayakodigesoftware.com>
2023-12-22 08:55:38 +0100

    Trying to fix failing login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
-// pages/_app.tsx
-import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
-import config from '@/amplifyconfiguration.json';
-import '@aws-amplify/ui-react/styles.css';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
-
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from '../amplifyconfiguration.json';
 Amplify.configure(config);
 
-function App({ Component, pageProps, signOut, user }: WithAuthenticatorProps) {
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
     <>
       <h1>Hello {user?.username}</h1>
       <button onClick={signOut}>Sign out</button>
-      <Component {...pageProps} />
     </>
   );
 }
